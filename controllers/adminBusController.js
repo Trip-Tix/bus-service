77867abe52988a8c4f605b5037c20b6ecf5d8693
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2023-08-07 01:33:42 +0600

    Layout add api fixed

--- controllers/adminBusController.js
+++ controllers/adminBusController.js
@@ -106,13 +106,26 @@ const getBusInfo = async (req, res) => {
 // Add bus layout info with bus id, coach id and layout details
 const addBusLayoutInfo = async (req, res) => {
     try {
+        // Begin transaction
+        await pool.query('BEGIN');
         console.log("addBusLayoutInfo called from bus-service");
         console.log(req.body);
         const {busId, coachId, numberOfSeats, matrix, matrixRows, matrixCols} = req.body;
         
+        // Check if bus layout already exists
+        const checkQuery = {
+            text: 'SELECT * FROM bus_layout_info WHERE bus_id = $1 AND coach_id = $2',
+            values: [busId, coachId]
+        };
+        const checkResult = await pool.query(checkQuery);
+        if (checkResult.rows.length > 0) {
+            console.log("Bus layout already exists");
+            return res.status(400).json({ message: 'Bus layout already exists' });
+        }
 
+        // Add bus layout info
         const query = {
-            text: 'INSERT INTO bus_layout (bus_id, coach_id, number_of_seats, matrix_rows, matrix_cols) VALUES ($1, $2, $3, $4, $5)',
+            text: 'INSERT INTO bus_layout_info (bus_id, coach_id, number_of_seats, matrix_rows, matrix_cols) VALUES ($1, $2, $3, $4, $5)',
             values: [busId, coachId, numberOfSeats, matrixRows, matrixCols]
         };
         await pool.query(query);
@@ -120,7 +133,7 @@ const addBusLayoutInfo = async (req, res) => {
 
         // Get the bus layout id
         const getQuery = {
-            text: 'SELECT * FROM bus_layout WHERE bus_id = $1 AND coach_id = $2',
+            text: 'SELECT * FROM bus_layout_info WHERE bus_id = $1 AND coach_id = $2',
             values: [busId, coachId]
         };
         const getResult = await pool.query(getQuery);
@@ -128,13 +141,15 @@ const addBusLayoutInfo = async (req, res) => {
 
         // Add layout details
         for (let i = 0; i < matrixRows; i++) {
+            let colCount = 0;
             for (let j = 0; j < matrixCols; j++) {
-                const seat_name = ''
+                let seat_name = ''
                 if (matrix[i][j] !== 0) {
-                    seat_name = String.fromCharCode(65 + i) + (j + 1);
+                    seat_name = String.fromCharCode(65 + i) + (colCount + 1);
+                    colCount++;
                 }
                 const query = {
-                    text: 'INSERT INTO bus_layout_details (bus_layout_id, seat_name, booked_status, is_seat, matrix_row_id, matrix_col_id) VALUES ($1, $2, $3, $4, $5, $6)',
+                    text: 'INSERT INTO bus_seat_details (bus_layout_id, seat_name, booked_status, is_seat, matrix_row_id, matrix_col_id) VALUES ($1, $2, $3, $4, $5, $6)',
                     values: [busLayoutId, seat_name, 0, matrix[i][j], i, j]
                 };
                 await pool.query(query);
@@ -144,7 +159,13 @@ const addBusLayoutInfo = async (req, res) => {
         console.log("Bus layout details added");
         res.status(200).json({ message: 'Bus layout added' });
     } catch (error) {
+        console.log(error);
+        // Rollback transaction
+        await pool.query('ROLLBACK');
         res.status(500).json({ message: error.message });
+    } finally {
+        // End transaction
+        await pool.query('COMMIT');
     }
 }
 
