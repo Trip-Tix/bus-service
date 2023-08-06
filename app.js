d8fc8f2de24b59632bff847782fe89054ae536b4
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2023-08-07 00:54:12 +0600

    Log updated

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/', router);
 
+app.get('/', (req, res) => {
+    res.send('Bus service is up and running');
+});
+
 app.listen(port, () => {
-    console.log(`Account service listening on port ${port}`);
+    console.log(`Bus service listening on port ${port}`);
 });
